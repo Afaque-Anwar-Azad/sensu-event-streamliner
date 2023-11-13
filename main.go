12177da3a55a8b4c957085b6fea349f7c39bd9ff
afaque <afaque@clevertap.com>
2023-11-14 02:55:18 +0530

    Adding sensu entity based event mutator

--- main.go
+++ main.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"github.com/sensu-community/sensu-plugin-sdk/sensu"
 	"github.com/sensu/sensu-go/types"
+	"io/ioutil"
+	"net/http"
+	"os"
 )
 
 // Config represents the mutator plugin config.
@@ -29,15 +34,58 @@ func checkArgs(_ *types.Event) error {
 	return nil
 }
 
+func handleError(message string, err error) {
+	fmt.Printf("%s: %s\n", message, err)
+	os.Exit(1)
+}
 func executeMutator(event *types.Event) (*types.Event, error) {
 
-	event.Entity.Redact = nil
-	event.Entity.System.Network.Interfaces = nil
-	event.Entity.Subscriptions = nil
-	event.Check.Handlers = nil
-	event.Check.History = nil
-	event.Check.RuntimeAssets = nil
-	event.Check.Subscriptions = nil
+	namespace, entityName := "default", event.Entity.Name
+	url := fmt.Sprintf("%s/api/core/v2/namespaces/%s/entities/%s", os.Getenv("SENSU_API_URL"), namespace, entityName)
+
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		handleError("Error creating request", err)
+	}
+	req.Header.Add("Authorization", "Key "+os.Getenv("SENSU_API_KEY"))
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		handleError("Error making request", err)
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		handleError("Error reading response body", err)
+	}
+
+	var result map[string]json.RawMessage
+	err = json.Unmarshal(body, &result)
+	if err != nil {
+		handleError("Error parsing JSON response", err)
+	}
+
+	var metadataLabels map[string]interface{}
+	err = json.Unmarshal(result["metadata"], &metadataLabels)
+	if err != nil {
+		handleError("Error extracting metadata from response", err)
+	}
+
+	labels, ok := metadataLabels["labels"].(map[string]interface{})
+	if !ok {
+		handleError("Error extracting labels from metadata", fmt.Errorf("labels field not found"))
+	}
+	jsonLabels, err := json.Marshal(labels)
+	if err != nil {
+		handleError("Error encoding labels to JSON", err)
+	}
+
+	var finalLabels map[string]string
+	err = json.Unmarshal([]byte(jsonLabels), &finalLabels)
+
+	event.Entity.Labels = finalLabels
 
 	return event, nil
 }
